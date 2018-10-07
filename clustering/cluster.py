c6276a84a8ad36149bbdb4937bcef4e3311aa82d
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-07 18:02:19 +1100

    MCL With File Data

--- clustering/cluster.py
+++ clustering/cluster.py
@@ -35,7 +35,8 @@ network = nx.random_geometric_graph(numnodes, 0.3, pos=positions)
 # then get the adjacency matrix (in sparse form)
 matrix = nx.to_scipy_sparse_matrix(network)
 
-result = mc.run_mcl(matrix, inflation=1.4)
+# result = mc.run_mcl(matrix, inflation=1.4)
+result = mc.run_mcl(matrix)
 clusters = mc.get_clusters(result)
 mc.draw_graph(matrix, clusters, pos=positions, node_size=50, with_labels=False, edge_color="silver")
 
