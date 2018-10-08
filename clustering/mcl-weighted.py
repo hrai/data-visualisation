1a16d1e3ccdf6647cd03693e79da70636d8f6c0f
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-08 19:05:11 +1100

    Added Second Mcl-weighted Files

--- clustering/mcl-weighted.py
+++ clustering/mcl-weighted.py
@@ -1,27 +1,39 @@
+import markov_clustering as mc
 import pandas as pd
-import numpy as np
 import networkx as nx
+import random
 import matplotlib.pyplot as plt
-# import pygraphviz_layout
- 
-# Build a dataframe with your connections
-df = pd.read_csv('data-weight.csv')
- 
-# Build your graph
-G=nx.from_pandas_edgelist(df, 'from', 'to', ['weight'])
-print(G.edges(data=True))
- 
-# Spring
-nx.draw(
-G,
-with_labels=True,
-node_size=1000,
-node_color="skyblue",
-arrows=True,
-arrowsize=15,
-# pos=nx.spring_layout(G, k=15, iterations=20))
-pos=nx.spring_layout(G))
 
-plt.title("spring")
+# number of nodes to use
+numnodes = 0
+
+df = pd.read_csv('data.csv')
+
+# generate random positions as a dictionary where the key is the node id and the value
+# is a tuple containing 2D coordinates
+# positions = {i:(random.random() * 2 - 1, random.random() * 2 - 1) for i in range(numnodes)}
+
+positions= {}
+with open('data.csv') as f:
+    for index, line in enumerate(f):
+        (key, val) = line.split(",")
+        positions[index] = (int(key), int(val))
+        numnodes = index
+
+# for i in positions:
+#     print(i)
+#     print(positions[i])
+
+print(numnodes)
+# use networkx to generate the graph
+network = nx.random_geometric_graph(numnodes, 4, pos=positions)
+
+# then get the adjacency matrix (in sparse form)
+matrix = nx.to_scipy_sparse_matrix(network)
+
+# result = mc.run_mcl(matrix, inflation=1.4)
+result = mc.run_mcl(matrix)
+clusters = mc.get_clusters(result)
+mc.draw_graph(matrix, clusters, pos=positions, node_size=50, with_labels=False, edge_color="silver")
 
 plt.show()
