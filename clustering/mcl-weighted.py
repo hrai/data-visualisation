dd8ecd644bfd5f466eda55e71bbe6c6d9769cd6f
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-09 18:46:30 +1100

    updated inflation value

--- clustering/mcl-weighted.py
+++ clustering/mcl-weighted.py
@@ -13,8 +13,8 @@ with open('data-weight.csv','rb') as f:
 # then get the adjacency matrix (in sparse form)
 matrix = nx.to_scipy_sparse_matrix(G, weight='weight')
 
-# result = mc.run_mcl(matrix, inflation=1.4)
-result = mc.run_mcl(matrix)  
+result = mc.run_mcl(matrix, inflation=2.5)
+# result = mc.run_mcl(matrix)  
 clusters = mc.get_clusters(result)
 
 mc.draw_graph(matrix, clusters,  node_size=50, with_labels=False, edge_color="silver")
